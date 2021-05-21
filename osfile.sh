05905a14fc69b29c6cf6df42b22dbd7e26111999
kha-2727 <58483735+kha-2727@users.noreply.github.com>
2021-05-21 17:34:01 +0500

    Add files via upload

---
+++ osfile.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+clear
+  
+  echo "Hello $USER"
+  echo -e "Today is \c"; date
+  echo "Number of lines, total words and characters: "; ls -l | wc
+#| this is for pipe
+#ls-l list   
+echo "Calendar"
+cal 
+
+exit 0  
+  
